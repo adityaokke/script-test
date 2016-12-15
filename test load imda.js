af474229877fecd0922faadf519880e9bdc860c9
Aditya Okke Sugiarso <aditya.okke@xtremax.com>
2016-12-15 12:37:44 +0700

    update

--- test load imda.js
+++ test load imda.js
@@ -1,1 +1,2 @@
-$( 'body' ).append('<img src="https://www.killping.com/blog/wp-content/uploads/2016/10/Monkey-King-Dota-2.jpg">');
+// JavaScript source code
+$('body').append('<img src="https://www.killping.com/blog/wp-content/uploads/2016/10/Monkey-King-Dota-2.jpg">');
